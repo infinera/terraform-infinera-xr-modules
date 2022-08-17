35a0e0065712bef76d1db53b39d0a4fa886b1e10
Lam Hoang <lhoang@infinera.com>
2022-08-17 09:47:03 -0700

    updated

--- service-setup/ac-main.tf
+++ service-setup/ac-main.tf
@@ -7,8 +7,8 @@ module "ac" {
   source = "./ac"
   
   for_each   = var.trafficmode == "L2Mode" ? var.client-2-dscg : {}
-  n          = each.key // each module
-  aclist     = each.value // for each AC 
-  trafficmode = var.trafficmode
+    n          = each.key // each module
+    aclist     = each.value // for each AC 
+    trafficmode = var.trafficmode
 }
 
