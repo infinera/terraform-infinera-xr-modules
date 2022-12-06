e844c8484eda2fd26dd688f0386dbf8d0bf666ee
Lam Hoang <lhoang@infinera.com>
2022-12-06 14:42:22 -0800

    updtaed

--- tasks/network/required-provider.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    xrcm = {
-      source = "infinera.com/poc/xrcm"
-    }
-  }
-}
--- tasks/network_attrbute_check/main.tf
+++ tasks/network_attrbute_check/main.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_providers {
-    xrcm = {
-      source = "infinera.com/poc/xrcm"
-    }
-  }
-}
 
 module  "get_and_filter_checked_resources"{
   source = "git::https://github.com/infinera/terraform-infinera-xr-modules.git//utils/get_and_filter_checked_resources"
