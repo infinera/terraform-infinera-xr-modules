dae69397daa94f0bcbcca06ca9b199189848fbdb
Lam Hoang <lhoang@infinera.com>
2022-12-06 11:14:54 -0800

    updated

--- utils/get_devices_with_different_ids/main.tf
+++ utils/get_devices_with_different_ids/main.tf
@@ -17,6 +17,7 @@ locals {
  devices  = data.xrcm_tfstate_devices_ids.devices_ids.device_ids != null ? {for device in data.xrcm_tfstate_devices_ids.devices_ids.device_ids : device.n => device} : null
 }
 
+
 locals {
   deviceid_checks = local.devices != null ? {for device in data.xrcm_devices.devices.devices: device.n => {tfstate_deviceid = local.devices[device.n].id, network_deviceid = device.deviceid} if device.deviceid != local.devices[device.n].id } : null
 }
