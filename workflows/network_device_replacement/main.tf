9e0d08781bb60b22b470142c237b68494da25763
Lam Hoang <lhoang@infinera.com>
2022-12-06 11:43:20 -0800

    updated

--- workflows/network_device_replacement/main.tf
+++ workflows/network_device_replacement/main.tf
@@ -23,3 +23,14 @@ module "network" {
   client-2-dscg     = var.client-2-dscg
   filtered_devices = module.network_with_IDs_check.device_names
 }
+
+module "network" {
+  depends_on = [module.network_with_IDs_check]
+  source = "git::https://github.com/infinera/terraform-infinera-xr-modules.git//tasks/network"
+  //source = "source = "../../tasks/network"
+  count = length(module.network_with_IDs_check.device_names) > 0 ? 1 : 0
+  network = var.network
+  leaf_bandwidth = var.leaf_bandwidth
+  hub_bandwidth = var.hub_bandwidth
+  client-2-dscg     = var.client-2-dscg
+}
